204c3b79aa501dff944eda5fe071a6b120d789ae
darian <xxy.moming@gmail.com>
2024-04-18 10:47:01 +0800

    Initial Commit

---
+++ Birthdays/BirthdaysApp.swift
@@ -0,0 +1,17 @@
+//
+//  BirthdaysApp.swift
+//  Birthdays
+//
+//  Created by Darian Mitchell  on 2024/4/18.
+//
+
+import SwiftUI
+
+@main
+struct BirthdaysApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
